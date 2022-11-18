2d2bd34a305497f362ac2ccd18a4d06fa5e288fe
SundarBharani <sundarbharani96@gmail.com>
2022-11-18 22:42:55 +0000

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLOPS End To End Implementation- Machine Learning',
+    author='sundarbharani',
+    license='MIT',
+)
